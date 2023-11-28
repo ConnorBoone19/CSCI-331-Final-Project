2e7b034620883f92e214a3cd7c21bc4ed59c2e6f
amandajg <100507834+amandajg@users.noreply.github.com>
2023-11-28 12:40:53 -0700

    Formats data from fakestoreapi

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-getBtn.addEventListener("click", fetchAPIData);
 let url = "https://fakestoreapi.com/products"
 
 fetchAPIData(); // hoisting will ensure that functions are defined before this call
@@ -7,10 +6,16 @@ async function fetchAPIData() {
     const response = await fetch(url);
     const items = await response.json();
     apiData = document.getElementById("apiData")
-    items.forEach( product => apiData.innerHTML += `<div id="product"
-                                                <p style="fontSize:.5rem">${product.title}</p>
-                                                <p>${product.price}</p>
-                                                <img style="width:100px" src=${product.image} alt=${product.title} />
+    items.forEach( product => apiData.innerHTML += `<div class="product">
+                                                <div class="productImage">
+                                                    <img style="width:100px" src=${product.image} alt=${product.title} />
+                                                </div>
+                                                <div class="productInfo">
+                                                    <div id="productTitle">
+                                                        <p style="fontSize:.5rem">${product.title}</p>
+                                                    </div>
+                                                    <p>$${product.price}</p>
+                                                </div>
                                                 </div>`)
 
 }
